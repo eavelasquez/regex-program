a0944efe695cb6b10d90e22848a5101a3a264c22
Esteban Velasquez <eavc.30@gmail.com>
2022-06-25 17:58:23 -0500

    feat: initial commit

---
+++ src/regexprogram/RegexProgram.java
@@ -0,0 +1,29 @@
+/******************************************************************************
+ * Compilation: javac RegexProgram.java
+ * Execution: java RegexProgram
+ *
+ * Reads in a string and prints out whether it matches the regular expression.
+ *
+ * % java RegexProgram
+ *
+ ******************************************************************************/
+package regexprogram;
+
+/**
+ * The {@code RegexProgram} class represents a program that reads in a string
+ * and prints out whether it matches the regular expression.
+ *
+ * @author eavelasquez
+ */
+public class RegexProgram {
+
+    /**
+     * Unit tests the {@code RegexProgram} data type.
+     *
+     * @param args the command-line arguments (none)
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
